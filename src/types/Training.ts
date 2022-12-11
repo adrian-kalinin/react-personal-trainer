d75ee3e327358a032bd5b44bba2e6b9480920450
Adrian Kalinin <adriankalinin@protonmail.com>
2022-12-11 16:36:57 +0200

    Update Training type

--- src/types/Training.ts
+++ src/types/Training.ts
@@ -1,9 +1,13 @@
+import Link from "./Link";
+
 type Training = {
-  id: number;
+  id?: number;
   date: string;
   duration: number;
   activity: string;
   customer: number;
+  content: any[];
+  links: Link[];
 };
 
 export default Training;
