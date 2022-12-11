122a8c1b4cdf3de0f19462598ef150a524bd968b
Adrian Kalinin <adriankalinin@protonmail.com>
2022-12-11 04:32:09 +0200

    Create types

---
+++ src/types/Customer.ts
@@ -0,0 +1,12 @@
+type Customer = {
+  id: number;
+  firstname: string;
+  lastname: string;
+  streetaddress: string;
+  postcode: string;
+  city: string;
+  email: string;
+  phone: string;
+};
+
+export default Customer;
---
+++ src/types/Training.ts
@@ -0,0 +1,9 @@
+type Training = {
+  id: number;
+  date: string;
+  duration: number;
+  activity: string;
+  customer: number;
+};
+
+export default Training;
