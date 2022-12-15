2930cea41dcfbb88a441744215b45f971dae6a1f
Adrian Kalinin <adriankalinin@protonmail.com>
2022-12-15 11:57:40 +0200

    Prepare for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,22 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/react-personal-trainer/",
+  build: {
+    rollupOptions: {
+      output: {
+        // eslint-disable-next-line consistent-return
+        manualChunks(id) {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
