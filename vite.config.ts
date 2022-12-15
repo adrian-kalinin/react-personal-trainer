2de1df580fb4b13a2ff4547b70b3ef261fc4cdb7
Adrian Kalinin <adriankalinin@protonmail.com>
2022-12-15 12:13:27 +0200

    Prepare for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/react-personal-trainer/",
   build: {
     rollupOptions: {
       output: {
