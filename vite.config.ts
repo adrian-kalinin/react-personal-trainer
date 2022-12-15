8f6f971ed414573ac4ce6b0faff23938b36fb191
Adrian Kalinin <adriankalinin@protonmail.com>
2022-12-15 12:10:11 +0200

    Prepare for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    base: "/",
     rollupOptions: {
       output: {
         // eslint-disable-next-line consistent-return
